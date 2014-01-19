e2118fd7c19ab354b665e0d29cb024d3e2e31e28
Horacio Herrera <hherrerag@me.com>
2014-01-19 21:58:36 +0100

    grunt setup

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,22 +3,35 @@ module.exports = function(grunt) {
   // Project configuration.
   grunt.initConfig({
     //global variables
-    appFolder: 'source',
+    srcFolder: 'source',
+    devFolder: 'dev',
 		buildFolder: 'build',
 		port: 4567,
 
     // Tasks setup.
     clean: {
+      dev: ['<%= devFolder %>'],
       prod: ['<%= buildFolder %>']
+    },
+
+    concurrent: {
+      dev: ['haml:dev', 'sass:dev'],
+      prod: ['haml:prod', 'sass:prod']
     },
 
     connect: {
       dev: {
         options: {
           port: '<%= port %>',
-          base: '<%= appFolder %>/',
+          base: '<%= devFolder %>/',
           hostname: 'localhost'
         }
+      }
+    },
+
+    open: {
+      server: {
+        path: 'http://localhost:<%= connect.dev.options.port %>'
       }
     },
 
@@ -30,23 +43,56 @@ module.exports = function(grunt) {
 		},
 
 		haml: {
-      prod: {
-        files: {
-          'main.html': 'main.haml',
-          'widgets.html': 'widgets.haml'
-        }
-      },
       dev: {
         options: {
           style: 'expanded'
         },
         files: {
-          'main.html': 'main.haml',
-          'widgets.html': [
-            'button.haml',
-            'tab.haml',
-            'debug.haml'  // Maybe you need one extra file in dev
-          ]
+          '<%= devFolder %>/index.html': '<%= srcFolder %>/index.haml'
+        }
+      },
+      prod: {
+        files: {
+          '<%= buildFolder %>/index.html': '<%= srcFolder %>/index.haml'
+        }
+      }
+    },
+
+    sass: {
+      dev: {
+        options: {
+          style: 'expanded',
+          lineNumbers: true
+        },
+        files: {
+          '<%= devFolder %>/css/main.css': '<%= srcFolder %>/css/main.scss'
+        }
+      },
+
+      prod: {
+        options: {
+          style: 'compressed',
+          lineNumbers: false
+        },
+        files: {
+          '<%= buildFolder %>/css/main.min.css': '<%= srcFolder %>/css/main.scss'
+        }
+      }
+    },
+
+    watch: {
+      html: {
+        files: ['<%= srcFolder %>/index.haml'],
+        tasks: ['haml:dev'],
+        options: {
+          livereload: true,
+        }
+      },
+      css: {
+        files: ['<%= devFolder %>/stylesheets/main.css', '<%= srcFolder %>/stylesheets/main.scss'],
+        tasks: ['sass:dev'],
+        options: {
+          livereload: true,
         }
       }
     },
@@ -54,18 +100,17 @@ module.exports = function(grunt) {
     ftpush: {
       build: {
         auth: {
-          host: '',
-          port: ,
+          host: 's123384.gridserver.com',
+          port: '21',
           authKey: 'key1'
         },
-        src: 'path/to/source/folder',
-        dest: '/path/to/destination/folder',
+        src: '<%= devFolder %>',
+        dest: 'domains/hherrerag.com/html/grunt',
         exclusions: [
-          'path/to/source/folder/**/.DS_Store',
-          'path/to/source/folder/**/Thumbs.db',
-          'dist/tmp'
-        ],
-        keep: ['/important/images/at/server/*.jpg']
+          '<%= devFolder %>/components/',
+          '<%= buildFolder %>/**/.DS_Store',
+          '<%= buildFolder %>/**/Thumbs.db'
+        ]
       }
     }
 
@@ -76,17 +121,19 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-haml');
   grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-concurrent');
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-connect');
   grunt.loadNpmTasks('grunt-open');
   grunt.loadNpmTasks('grunt-uncss');
+  grunt.loadNpmTasks('grunt-processhtml');
   grunt.loadNpmTasks('grunt-contrib-uglify');
   grunt.loadNpmTasks('grunt-contrib-ftpush');
 
   // Default task.
   grunt.registerTask('default', []);
-  grunt.registerTask('server', []);
-  grunt.registerTask('build', []);
-  grunt.registerTask('deploy', []);
+  grunt.registerTask('server', ['clean:dev', 'concurrent:dev', 'connect:dev', 'open', 'watch']);
+  grunt.registerTask('build', ['clean:prod', 'concurrent:prod']);
+  grunt.registerTask('deploy', ['ftpush']);
 
 };
